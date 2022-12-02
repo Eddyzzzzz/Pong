08acceb80cbf805999d88135299faaf9330e226f
Eddyzzzzz <90101497+Eddyzzzzz@users.noreply.github.com>
2022-12-01 19:16:29 -0500

    Update NES.vhd
    Update to two 2-bit outputs ("up" and "down" for both controllers) and a 1-bit output (starting the game)

--- NES.vhd
+++ NES.vhd
@@ -1,18 +1,25 @@
 library IEEE;
 use IEEE.std_logic_1164.all; use IEEE.numeric_std.all;
 
-entity top is 
+entity nes is 
   port(
-    latch : out std_logic;
-    clock : out std_logic;
-    data : in std_logic;
-    up : out std_logic;
-    down : out std_logic;
+    latch1 : out std_logic;
+    latch2 : out std_logic;
+    
+    clock1 : out std_logic;
+    clock2 : out std_logic;
+    
+    data1 : in std_logic;
+    data2 : in std_logic;
+    
+    nes1 : out std_logic_vector(1 downto 0); --bit 1 : up; bit 2 : down
+    nes2 : out std_logic_vector(1 downto 0); --bit 1 : up; bit 2 : down
+    
     start : out std_logic
   );
-end top;
+end nes;
   
-architecture synth of top is
+architecture synth of nes is
   component HSOSC is generic (
     CLKHF_DIV : String := "0b00"); port(
     CLKHFPU : in std_logic := 'X'; CLKHFEN : in std_logic := 'X'; CLKHF : out std_logic := 'X');
@@ -23,7 +30,12 @@ architecture synth of top is
   -- 48 MHz * 25ms = 1200000 = 2^20
   signal counter : unsigned (20 downto 0) := 21d"0"; signal NESclk : std_logic;
   signal NEScount : unsigned (7 downto 0);
-  signal output : std_logic_vector(7 downto 0):= 8d"0";
+
+  signal output1 : std_logic_vector(7 downto 0):= 8d"0";
+  signal output2 : std_logic_vector(7 downto 0):= 8d"0";
+
+  signal start1 : std_logic;
+  signal start2 : std_logic;
 
 begin
   
@@ -42,22 +54,38 @@ begin
     
   NEScount <= counter(16 downto 9);
     
-  latch <= '1' when NEScount = "11111111" else '0';
+  latch1 <= '1' when NEScount = "11111111" else '0';
+  latch2 <= '1' when NEScount = "11111111" else '0';
     
-  clock <= NESclk when NEScount < "00001000" else '0';
+  clock1 <= NESclk when NEScount < "00001000" else '0';
+  clock2 <= NESclk when NEScount < "00001000" else '0';
     
-  process (clock) begin
-    if rising_edge(clock) then
-      output(0) <= output(1); output(1) <= output(2); output(2) <= output(3); output(3) <= output(4); output(4) <= output(5); output(5) <= output(6); output(6) <= output(7); output(7) <= data;
+  process (clock1) begin
+    if rising_edge(clock1) then
+      output1(0) <= output1(1); output1(1) <= output1(2); output1(2) <= output1(3); output1(3) <= output1(4); output1(4) <= output1(5); output1(5) <= output1(6); output1(6) <= output1(7); output1(7) <= data1;
     end if; 
   end process;
       
-  process (latch) begin
-    if rising_edge(latch) then
-      up <= output(5); 
-      down <= output(6);
-      start <= output(2);
+  process (clock2) begin
+    if rising_edge(clock2) then
+      output2(0) <= output2(1); output2(1) <= output2(2); output2(2) <= output2(3); output2(3) <= output2(4); output2(4) <= output2(5); output2(5) <= output2(6); output2(6) <= output2(7); output2(7) <= data2;
+    end if; 
+  end process;
+      
+  process (latch1) begin
+    if rising_edge(latch1) then
+      nes1 <= output1(5) & output1(6); 
+      start1 <= output1(2);
     end if;
   end process; 
+ 
+  process (latch2) begin
+    if rising_edge(latch2) then
+      nes2 <= output2(5) & output2(6); 
+      start2 <= output2(2);
+    end if;
+  end process; 
+      
+  start <= start1 or start2;
       
 end;
