bff2597f8f0882f8f1d78c6a160706c46a906867
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2022-12-02 18:12:00 -0500

    Used the variable names from the document

--- Paddle.vhd
+++ Paddle.vhd
@@ -4,13 +4,13 @@ use IEEE.numeric_std.all;
 
 entity Paddle is
 port(
-    clk     : in std_logic;
-    p1Delta : in std_logic_vector(1 downto 0);
-    p2Delta : in std_logic_vector(1 downto 0);
-    restart : in std_logic;
+    clk    : in std_logic;
+    p1Move : in std_logic_vector(1 downto 0);
+    p2Move : in std_logic_vector(1 downto 0);
+    state  : in std_logic;
     
-    pad1Pos : out unsigned(9 downto 0);
-    pad2Pos : out unsigned(9 downto 0)
+    p1Pos : out unsigned(9 downto 0);
+    p2Pos : out unsigned(9 downto 0)
 );
 end;
     
@@ -24,21 +24,21 @@ begin
     begin
         
         if rising_edge(clk) then
-            if restart = '0' then
+            if state = '0' then
                 pad1  <= 10b"11110000";
                 pad2  <= 10b"11110000";
                 delay <= 21b"0";
             else
                 if delay = 21b"0" then
-                    if p1Delta(1) = '1' then
+                    if p1Move(1) = '1' then
                         pad1 <= pad1 + 10b"1" when pad1 < 413 else pad1;
-                    elsif p1Delta(0) = '1' then
+                    elsif p1Move(0) = '1' then
                         pad1 <= pad1 + 10b"1111111111" when pad1 > 67 else pad1;
                     end if;
                     
-                    if p2Delta(1) = '1' then
+                    if p2Move(1) = '1' then
                         pad2 <= pad2 + 10b"1" when pad2 < 413 else pad2;
-                    elsif p2Delta(0) = '1' then
+                    elsif p2Move(0) = '1' then
                         pad2 <= pad2 + 10b"1111111111" when pad2 > 67 else pad2;
                     end if;
                 end if;
@@ -48,8 +48,7 @@ begin
         end if;
     end process;
     
-    pad1Pos <= pad1;
-    pad2Pos <= pad2;
-    
-end;
+    p1Pos <= pad1;
+    p2Pos <= pad2;
 
+end;
