96b9bc000ffeeacce1043240d39d4d15c74eb000
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2022-12-02 13:58:13 -0500

    Added a timer delay

--- Paddle.vhd
+++ Paddle.vhd
@@ -15,6 +15,7 @@ port(
 end;
     
 architecture synth of Paddle is
+    signal delay   : unsigned(20 downto 0);
     signal pad1    : unsigned(9 downto 0);
     signal pad2    : unsigned(9 downto 0);
 begin
@@ -24,22 +25,26 @@ begin
         
         if rising_edge(clk) then
             if restart = '0' then
-                pad1 <= 10b"11110000";
-                pad2 <= 10b"11110000";
+                pad1  <= 10b"11110000";
+                pad2  <= 10b"11110000";
+                delay <= 21b"0";
             else
-                if p1Delta(1) = '1' then
-                    pad1 <= pad1 + 10b"1" when pad1 < 413 else pad1;
-                elsif p1Delta(0) = '1' then
-                    pad1 <= pad1 + 10b"1111111111" when pad1 > 67 else pad1;
+                if delay = 21b"0" then
+                    if p1Delta(1) = '1' then
+                        pad1 <= pad1 + 10b"1" when pad1 < 413 else pad1;
+                    elsif p1Delta(0) = '1' then
+                        pad1 <= pad1 + 10b"1111111111" when pad1 > 67 else pad1;
+                    end if;
+                    
+                    if p2Delta(1) = '1' then
+                        pad2 <= pad2 + 10b"1" when pad2 < 413 else pad2;
+                    elsif p2Delta(0) = '1' then
+                        pad2 <= pad2 + 10b"1111111111" when pad2 > 67 else pad2;
+                    end if;
                 end if;
-                
-                if p2Delta(1) = '1' then
-                    pad2 <= pad2 + 10b"1" when pad2 < 413 else pad2;
-                elsif p2Delta(0) = '1' then
-                    pad2 <= pad2 + 10b"1111111111" when pad2 > 67 else pad2;
-                end if;
+                delay <= delay + 21b"1";
             end if;
-            
+                
         end if;
     end process;
     
