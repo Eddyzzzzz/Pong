5c1b6e13294df3cf6d2dc248cae46c2a786765c5
JlBrockett <41646692+JlBrockett@users.noreply.github.com>
2022-12-05 12:32:30 -0500

    Update ball.vhd
    Changed variable names to match specs.

--- ball.vhd
+++ ball.vhd
@@ -8,10 +8,10 @@ port(
 
     p1pos : in unsigned (9 downto 0);
     p2pos : in unsigned (9 downto 0);
-    ballxpos : out unsigned(9 downto 0);
-    ballypos : out unsigned(9 downto 0);
+    xpos : out unsigned(9 downto 0);
+    ypos : out unsigned(9 downto 0);
     state : in std_logic;
-    score: out std_logic_vector(1 downto 0);
+    scored: out std_logic_vector(1 downto 0);
     
     clk: in std_logic
 );
@@ -23,28 +23,28 @@ signal interset : std_logic;
 
 begin
     interset <= '1' when state else
-    '1' when not score = "00" else '0';
+    '1' when not scored = "00" else '0';
 process (clk) 
     begin
     if rising_edge(clk) then
         ballvel(0) <= '0' when state else
-        '1' when ballxpos = d"15" and ((ballypos > p1pos - d"135") and (ballypos < p1pos)) else
-        '0' when ballxpos = d"625" and ((ballypos > p2pos - d"135") and (ballypos < p2pos)) else
+        '1' when xpos = d"15" and ((ypos > p1pos - d"135") and (ypos < p1pos)) else
+        '0' when xpos = d"625" and ((ypos > p2pos - d"135") and (ypos < p2pos)) else
         ballvel(0);
         ballvel(1) <= '0' when state else
-        '1' when ballypos = "0000000000" else
-        '0' when ballypos = 10d"480" else
+        '1' when ypos = "0000000000" else
+        '0' when ypos = 10d"480" else
         ballvel(1);
         
-        ballxpos <= 10d"320" when interset or state else
-            ballxpos+10d"1" when ballvel(0) else 
-            ballxpos-10d"1";
-        ballypos <= 10d"240" when interset or state else
-            ballypos+10d"1" when ballvel(1) else
-            ballypos-10d"1";
+        xpos <= 10d"320" when interset or state else
+            xpos+10d"1" when ballvel(0) else 
+            xpos-10d"1";
+        ypos <= 10d"240" when interset or state else
+            ypos+10d"1" when ballvel(1) else
+            ypos-10d"1";
         
-        score <= "01" when ballxpos = 10d"480" else
-        "10" when ballxpos = 10d"0" else
+        scored <= "01" when xpos = 10d"480" else
+        "10" when xpos = 10d"0" else
         "00";
         
     end if;
