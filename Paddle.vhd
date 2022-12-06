ca06b2bee2a3990a07fae9ea4b3f3e2760371ea3
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2022-12-05 23:10:26 -0500

    Update Paddle.vhd

--- Paddle.vhd
+++ Paddle.vhd
@@ -30,20 +30,20 @@ begin
                 delay <= 19b"0";
             else
                 if delay = 18b"0" then
-                    if p1Move(1) = '1' then
-                        pad1 <= pad1 + 10b"1" when pad1 < 135 else pad1;
-                    elsif p1Move(0) = '1' then
+                    if p1Move(0) = '0' then
+                        pad1 <= pad1 + 10b"1" when pad1 < 345 else pad1;
+                    elsif p1Move(1) = '0' then
                         pad1 <= pad1 - 10b"1" when pad1 > 0 else pad1;
-					-- else 
-					-- 	pad1 <= pad1;
+					 else 
+						pad1 <= pad1;
                     end if;
                     
-                    if p2Move(1) = '1' then
-                        pad2 <= pad2 + 10b"1" when pad2 < 135 else pad2;
-                    elsif p2Move(0) = '1' then
+                    if p2Move(0) = '0' then
+                        pad2 <= pad2 + 10b"1" when pad2 < 345 else pad2;
+                    elsif p2Move(1) = '0' then
                         pad2 <= pad2 - 10b"1" when pad2 > 0 else pad2;
-                    -- else 
-					-- 	pad2 <= pad2;
+                    else 
+					 	pad2 <= pad2;
 					end if;
 					
                 end if;
