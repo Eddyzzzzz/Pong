5710a132e256910e84d14142ac6ef1e87f5ffdbb
alko08 <32113635+alko08@users.noreply.github.com>
2022-12-06 16:10:25 -0500

    Added 0 to the screen for p1
    The code does this by creating a number that is 7 by 11 pixels (where each pixel is actually 5x5 actual pixels). It looks at each row of the number and determines what should be there based on what number it is. Only 0 atm.

--- pattern_gen.vhd
+++ pattern_gen.vhd
@@ -22,6 +22,7 @@ architecture sim of pattern_gen is
 --signal p2Pos : unsigned(9 downto 0);
 --signal xPos : unsigned(9 downto 0);
 --signal yPos : unsigned(9 downto 0);
+signal p1 : unsigned (3 downto 0);
 
 begin
 	-- For Testing screen.
@@ -29,6 +30,7 @@ begin
 --	p2Pos <= 10d"180";
 --	xPos <= 10d"320";
 --	yPos <= 10d"240";
+	p1 <= 4b"0";
 	
 	-- Make sure to minus 150 to any column calculation and 9 from any row calculation
 	rgb <= "000000" when (col < 150 or col > 795 or row < 10 or row > 489) else --Out of bounds area = black
@@ -36,7 +38,20 @@ begin
 		or (col-150 > 629 and row-9 >= p2Pos and row-9 < p2Pos + 135) -- Right Paddle
 		or (col-150 > xPos - 8 and col-150 < xPos + 8  and row-9 > yPos - 8 and row-9 < yPos + 8) -- Ball
 		or (col-150 > 318 and col-150 < 322 and row mod 6 < 3) -- Dotted Center Line
-		or (row < 11 or row > 488)) else "000000"; -- Top and bottom edges, else black
+		or (row < 11 or row > 488) -- Top and bottom edges, else black
+		
+		-- NUMBERS: Start at Row > 15, Row < 21, col < 306 for right pixel (p1), col > 337 for left pixel (p2), 5 per pixel, 
+		or (row-9 > 15 and row-9 < 21 and ((p1 = 0 or p1= 10)and col-150 < 301 and col-150 > 275))
+		or (row-9 > 20 and row-9 < 26 and ((p1 = 0 or p1= 10)and col-150 < 306 and col-150 > 270))
+		or (row-9 > 25 and row-9 < 31 and ((p1 = 0 or p1= 10)and ((col-150 < 306 and col-150 > 295) or (col-150 < 281 and col-150 > 270))))
+		or (row-9 > 30 and row-9 < 36 and ((p1 = 0 or p1= 10)and ((col-150 < 306 and col-150 > 295) or (col-150 < 281 and col-150 > 270))))
+		or (row-9 > 35 and row-9 < 41 and ((p1 = 0 or p1= 10)and ((col-150 < 306 and col-150 > 295) or (col-150 < 281 and col-150 > 270))))
+		or (row-9 > 40 and row-9 < 46 and ((p1 = 0 or p1= 10)and ((col-150 < 306 and col-150 > 295) or (col-150 < 281 and col-150 > 270))))
+		or (row-9 > 45 and row-9 < 51 and ((p1 = 0 or p1= 10)and ((col-150 < 306 and col-150 > 295) or (col-150 < 281 and col-150 > 270))))
+		or (row-9 > 50 and row-9 < 56 and ((p1 = 0 or p1= 10)and ((col-150 < 306 and col-150 > 295) or (col-150 < 281 and col-150 > 270))))
+		or (row-9 > 55 and row-9 < 61 and ((p1 = 0 or p1= 10)and ((col-150 < 306 and col-150 > 295) or (col-150 < 281 and col-150 > 270))))
+		or (row-9 > 60 and row-9 < 66 and ((p1 = 0 or p1= 10)and col-150 < 306 and col-150 > 270))
+		or (row-9 > 65 and row-9 < 71 and ((p1 = 0 or p1= 10)and col-150 < 301 and col-150 > 275))) else "000000";
 
 -- Good luck!
 	
