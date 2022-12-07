dc231d3f3ef2b8d2b93239f907fb0eba722eba10
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2022-12-07 15:41:55 -0500

    added some comments to Paddle

--- Paddle.vhd
+++ Paddle.vhd
@@ -24,12 +24,14 @@ begin
     begin
         
         if rising_edge(clk) then
+			-- Reset the state when in different state
             if state = '1' then
                 pad1  <= 10b"10100010";
                 pad2  <= 10b"10100010";
                 delay <= 17b"0";
             else
-                if delay = 17b"0" then
+                if delay = 17b"0" then -- On the "0th" frame, update
+					-- Player 1
                     if p1Move(0) = '0' then
                         pad1 <= pad1 + 10b"1" when pad1 < 345 else pad1;
                     elsif p1Move(1) = '0' then
@@ -38,6 +40,7 @@ begin
 						pad1 <= pad1;
                     end if;
                     
+					-- Player 2
                     if p2Move(0) = '0' then
                         pad2 <= pad2 + 10b"1" when pad2 < 345 else pad2;
                     elsif p2Move(1) = '0' then
@@ -47,7 +50,7 @@ begin
 					end if;
 					
                 end if;
-                delay <= delay + 17b"1";
+                delay <= delay + 17b"1"; -- Increment the timer
             end if;
                 
         end if;
