191e0398771cf19586077627ce871f9350296dbc
Eddyzzzzz <90101497+Eddyzzzzz@users.noreply.github.com>
2022-12-07 21:03:24 -0500

    Aline 'if' statement; no code is changed.

--- Paddle.vhd
+++ Paddle.vhd
@@ -24,33 +24,39 @@ begin
     begin
         
         if rising_edge(clk) then
-			-- Reset the state when in different state
+	    -- Reset the state when in different state
             if state = '1' then
+		    
                 pad1  <= 10b"10100010";
                 pad2  <= 10b"10100010";
                 delay <= 17b"0";
+	
             else
-                if delay = 17b"0" then -- On the "0th" frame, update
-					-- Player 1
+		    
+		-- On the "0th" frame, update    
+                if delay = 17b"0" then 
+		    -- Player 1
                     if p1Move(0) = '0' then
                         pad1 <= pad1 + 10b"1" when pad1 < 345 else pad1;
                     elsif p1Move(1) = '0' then
                         pad1 <= pad1 - 10b"1" when pad1 > 0 else pad1;
-					 else 
-						pad1 <= pad1;
+		    else 
+			pad1 <= pad1;
                     end if;
                     
-					-- Player 2
+		    -- Player 2
                     if p2Move(0) = '0' then
                         pad2 <= pad2 + 10b"1" when pad2 < 345 else pad2;
                     elsif p2Move(1) = '0' then
                         pad2 <= pad2 - 10b"1" when pad2 > 0 else pad2;
                     else 
-					 	pad2 <= pad2;
-					end if;
+			pad2 <= pad2;
+		    end if;
 					
                 end if;
+			
                 delay <= delay + 17b"1"; -- Increment the timer
+			    
             end if;
                 
         end if;
