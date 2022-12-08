557f2980f484672b73ffce5dff34ee8884eb270a
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2022-12-08 00:02:17 -0500

    add a delay after score

--- ball.vhd
+++ ball.vhd
@@ -18,10 +18,11 @@ port(
 end;
 
 architecture synth of ball is
-signal ballvel  : std_logic_vector(1 downto 0); --x, y
-signal interset : std_logic;
-signal delay    : unsigned(16 downto 0);
-signal waitVal  : unsigned(16 downto 0);
+signal ballvel    : std_logic_vector(1 downto 0); --x, y
+signal interset   : std_logic;
+signal delay      : unsigned(16 downto 0);
+signal waitVal    : unsigned(16 downto 0);
+signal afterScore : unsigned(13 downto 0);
 begin
     
 	process (clk) 
@@ -30,13 +31,16 @@ begin
 			-- Do the reset
 			interset <= '1' when (state = '1' or xpos < 10d"0" or xpos > 10d"640")
 						 else '0';
-			-- Slow down for humans
-			if delay = 17b"0" then
+			if afterScore > 14b"0" then
+				afterScore <= afterScore - 14b"1";
+			elsif delay = 17b"0" then -- Slow down for humans
 				-- Choose which direction the ball goes
+					-- x direction
 				ballvel(0) <= '0' when state else
 				'1' when xpos < 10d"15" and ((ypos < p1pos + d"135") and (ypos > p1pos)) else
 				'0' when xpos > 10d"625" and ((ypos < p2pos + d"135") and (ypos > p2pos)) else
 				ballvel(0);
+					-- y direction
 				ballvel(1) <= '0' when state else
 				'1' when ypos = "0000000011" else
 				'0' when ypos = 10d"477" else
@@ -56,7 +60,8 @@ begin
 				if (xpos < 10d"15" and ((ypos < p1pos + d"135") and (ypos > p1pos))) or (xpos > 10d"625" and ((ypos < p2pos + d"135") and (ypos > p2pos))) then
 					waitVal <= waitVal - 17d"1";
 				elsif xpos < 10d"0" or xpos > 10d"640" then
-					waitVal <= "11111111111111111";
+					waitVal <= 17b"11111111111111111";
+					afterScore <= 14b"11111111111111";
 				end if;
 				
 				-- Check when someone scores
@@ -64,7 +69,7 @@ begin
 				"10" when xpos < 10d"0" else
 				"00";
 			end if;
-			-- Increases the speed
+			-- Go through the delay
 			delay <= delay + 17b"1" when delay < waitVal else 17b"0";
 		end if;
 	end process;
