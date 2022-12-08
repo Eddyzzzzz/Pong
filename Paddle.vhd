4b69c79f03664da7056e83726e4de58a316b674b
Matthew Wong <44908570+richmanpoorman@users.noreply.github.com>
2022-12-07 21:07:02 -0500

    changed some spacing

--- Paddle.vhd
+++ Paddle.vhd
@@ -26,38 +26,34 @@ begin
         if rising_edge(clk) then
 	    -- Reset the state when in different state
             if state = '1' then
-		    
+				-- Start in the mid, and put the delay to 0
                 pad1  <= 10b"10100010";
                 pad2  <= 10b"10100010";
                 delay <= 17b"0";
 	
-            else
-		    
-		-- On the "0th" frame, update    
-                if delay = 17b"0" then 
-		    -- Player 1
-                    if p1Move(0) = '0' then
-                        pad1 <= pad1 + 10b"1" when pad1 < 345 else pad1;
-                    elsif p1Move(1) = '0' then
-                        pad1 <= pad1 - 10b"1" when pad1 > 0 else pad1;
-		    else 
-			pad1 <= pad1;
-                    end if;
-                    
-		    -- Player 2
-                    if p2Move(0) = '0' then
-                        pad2 <= pad2 + 10b"1" when pad2 < 345 else pad2;
-                    elsif p2Move(1) = '0' then
-                        pad2 <= pad2 - 10b"1" when pad2 > 0 else pad2;
-                    else 
-			pad2 <= pad2;
-		    end if;
-					
-                end if;
+            elsif delay = 17b"0" then 
 			
-                delay <= delay + 17b"1"; -- Increment the timer
-			    
-            end if;
+		    -- Player 1
+				if p1Move(0) = '0' then
+					pad1 <= pad1 + 10b"1" when pad1 < 345 else pad1;
+				elsif p1Move(1) = '0' then
+					pad1 <= pad1 - 10b"1" when pad1 > 0 else pad1;
+				else 
+					pad1 <= pad1;
+				end if;
+			
+		    -- Player 2
+				if p2Move(0) = '0' then
+					pad2 <= pad2 + 10b"1" when pad2 < 345 else pad2;
+				elsif p2Move(1) = '0' then
+					pad2 <= pad2 - 10b"1" when pad2 > 0 else pad2;
+				else 
+					pad2 <= pad2;
+				end if;
+					
+			end if;
+			
+			delay <= delay + 17b"1"; -- Increment the timer
                 
         end if;
     end process;
